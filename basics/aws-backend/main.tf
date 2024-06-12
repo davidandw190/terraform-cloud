6104b0e73556aa63afb7ad8e3d6864585a1a6ee0
Andrei-David Nan <nandavid190@gmail.com>
2024-06-12 17:59:08 +0300

    setup the aws-backend resources in basics dir, add architecture.png

---
+++ basics/aws-backend/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  #############################################################
+  ## AFTER RUNNING TERRAFORM APPLY (WITH LOCAL BACKEND)
+  ## THIS CODE WILL BE UNCOMMENTED, AND THEN RERUN WITH
+  ## TERRAFORM INIT TO SWITCH FROM LOCAL BACKEND TO REMOTE 
+  ## AWS BACKEND
+  #############################################################
+  # backend "s3" {
+  #   bucket         = "tf_cloud_state" # REPLACE ACTUAL BUCKET NAME
+  #   key            = "03-basics/import-bootstrap/terraform.tfstate"
+  #   region         = "us-east-1"
+  #   dynamodb_table = "terraform-state-locking"
+  #   encrypt        = true
+  # }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "tf-cloud-state" # REPLACE WITH ACTUAL BUCKET NAME
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
+
